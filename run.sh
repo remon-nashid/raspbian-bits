61755b307d60f5ae63791134f2d9282563d7305a
Remon Nashid <remon.sherin@gmail.com>
2019-04-09 16:25:21 -0400

    Add run and update scripts

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -d -p 3006:3006 -v config:/app/config --name=miner remonyesbit/miner:latest
---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+IMAGE="remonyesbit/miner"
+CID=$(docker ps | grep $IMAGE | awk '{print $1}')
+LATEST=`docker inspect --format "{{.Id}}" $IMAGE`
+RUNNING=`docker inspect --format "{{.Image}}" $CID`
+CONTAINER=`docker inspect --format '{{.Name}}' $CID | sed "s/\///g"`
+
+docker pull $IMAGE
+
+echo "IMAGE:" $IMAGE
+echo "CID:" $CID
+echo "RUNNING:" $RUNNING
+echo "LATEST:" $LATEST
+echo "CONTAINER:" $CONTAINER
+
+if [ "$RUNNING" != "$LATEST" ];then
+    echo "updating $CONTAINER"
+    docker stop $CONTAINER
+    docker wait $CONTAINER
+    docker rm -f $CONTAINER
+    ./run.sh
+else
+    echo "$CONTAINER is already up to date"
+fi
