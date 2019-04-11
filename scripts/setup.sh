e0e3eedb57cff518f029569cd76e16535c83489f
Remon Nashid <remon.sherin@gmail.com>
2019-04-11 14:48:12 -0400

    Almost working

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,43 @@
-# Docker
+#!/usr/bin/env bash
 
-sudo apt-get update
+#####################
+# System dependencies
+#####################
 
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
+echo "Checking dependencies."
+if [ "$(dpkg-query -W -f='${Status}' docker-ce 2>/dev/null | grep -c 'ok installed')" -eq 0 ]; then
+    echo "No docker-ce. Installing..."
+    sudo apt-get update
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg2 \
+        software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+"deb [arch=armhf] https://download.docker.com/linux/deb ian \
+$(lsb_release -cs) \
+stable"
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+else
+    echo "Dependencies had been installed."
+fi
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-   "deb [arch=armhf] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+#####################
+# Systemd files
+#####################
 
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+echo "Configuring Systemd..."
+# Create directory
+sudo mkdir -p /etc/systemd/system/latest.target.wants
+# Copy our service
+# sudo cp ./systemd/*.service /etc/systemd/system/latest.target.wants/
+sudo cp ./systemd/*.service /etc/systemd/system/
+# Run our target latest
+sudo systemctl isolate latest.target
+sudo ln -sf /etc/systemd/system/latest.target /etc/systemd/system/default.target
+# Enable service
+sudo systemctl enable --now miner-startup.service
+echo "Complete!"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
 IMAGE="remonyesbit/miner"
 
-docker run -d -p 3006:3006 -v config:/app/config --name=miner $IMAGE:latest
+# If image is there just start it
+# if it's started do nothing
+
+CID=$(docker ps -a | grep $IMAGE | awk '{print $1}')
+echo "CID: $CID"
+if [[ -z "$CID" ]] 
+then 
+    echo "No container found. Will start now."    
+    docker run -d -p 3006:3006 -v config:/app/config --name=miner $IMAGE:latest
+fi
+
+
+CONTAINER=$(docker inspect --format '{{.Name}}' "$CID" | sed "s/\///g")
+echo "CONTAINER: $CONTAINER"
+docker start "$CONTAINER"
